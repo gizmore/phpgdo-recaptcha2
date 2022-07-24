3cb5e0da45e0e078f78202ade901710d82706cad
gizmore <gizmore@wechall.net>
2022-07-24 22:13:05 +0200

    '7.0.1-r1401'

--- lang/captcha_en.php
+++ lang/captcha_en.php
@@ -3,7 +3,7 @@ return [
     'cfg_google_site_key' => 'Site-Key',
     'cfg_google_site_secret' => 'Site-Secret',
     
-    'ft_captcha_captchatest' => 'Captcha Test',
+    'mt_captcha_captchatest' => 'Captcha Test',
     'msg_test_passed' => 'The test was successful',
     'err_captcha_failed' => 'Sie müssen das Captcha lösen.',
 ];
